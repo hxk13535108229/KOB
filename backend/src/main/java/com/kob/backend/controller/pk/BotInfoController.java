c4da0e987587278e75595a9f8a55b32610a6fd37
asurahuang <asurahuang@tencent.com>
2022-10-25 20:54:43 +0800

    项目的åè骨架搭建

--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -3,6 +3,9 @@ package com.kob.backend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @ClassName BotInfoController
  * @Description TODO
@@ -15,7 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class BotInfoController {
 
     @RequestMapping("getBotInfo/")
-    public String getBotInfo() {
-        return "hhh";
+    public Map<String,String> getBotInfo() {
+        HashMap<String, String> bot1 = new HashMap<>();
+        bot1.put("name", "tiger");
+        bot1.put("ranking", "1500");
+        return bot1;
     }
 }
